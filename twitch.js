35114b957fcbf9e02b30749f97c9fe46bf65117f
Brittany <bmariedevelopment@gmail.com>
2017-06-05 21:38:09 -0600

    a third attempt at working on the online/offline problem

--- twitch.js
+++ twitch.js
@@ -3,7 +3,7 @@ var onlineUsers = [];
 var html = '';
 var onlineUsernames = [];
 
-function User(username, id, status, game, url, logo) 
+function User(username, id, status, game, url, logo, viewers) 
 	{
 		this.username = username;
 		this.id = id;
@@ -11,7 +11,8 @@ function User(username, id, status, game, url, logo)
 		this.game = game;
 		this.url = url;
 		this.logo = logo;
-		this.online = 0;
+		//option to set viewers equal to a certain number, and only show allUsers in the offline mode if viewers is equal to 0
+		this.viewers;
 	}
 
 //iterate user list
@@ -27,7 +28,7 @@ for (var i = 0; i < allUsers.length; i++) {
 			console.log(streams);
 			if(streams.stream !== null) {
 				var arr = streams.stream.channel;
-				onlineUsers.push(new User(arr.display_name, arr._id, arr.status, arr.game, arr.url, arr.logo, 1))
+				onlineUsers.push(new User(arr.display_name, arr._id, arr.status, arr.game, arr.url, arr.logo, streams.stream.viewers))
 				html = '<li class="online"><a href=' + arr.url + ' target="_blank"><img class="logo" src=' + arr.logo+ ">  " + arr.display_name + '</a> ' + arr.status + '</li>';
 	       		$(".users").append(html);
 	       		var name = arr.display_name;
@@ -37,22 +38,23 @@ for (var i = 0; i < allUsers.length; i++) {
 			} 
 		}
 	})
-	console.log(onlineUsernames);
-	//make offline users list from online users and all users
+	//option to make a new "offline users" array using onlineUsers and allUsers
+	// console.log(onlineUsernames);
+	// //make offline users list from online users and all users
 	var offlineUsers = [];
-	for (var k = 0; k < allUsers.length; k++) {
-	    if (onlineUsernames.indexOf(allUsers[k]) === -1){
-	      offlineUsers.push(allUsers[k]);
-	    }
-	  }
-	  console.log(offlineUsers);
-	  console.log(allUsers);
-	for (var j = 0; j < onlineUsers.length; j++) {
-	    if (allUsers.indexOf(onlineUsernames[j]) === -1){
-	      offlineUsers.push(onlineUsernames[j]);
-	    }
-	  }
-	console.log(offlineUsers)
+	// for (var k = 0; k < allUsers.length; k++) {
+	//     if (onlineUsernames.indexOf(allUsers[k]) === -1){
+	//       offlineUsers.push(allUsers[k]);
+	//     }
+	//   }
+	//   console.log(offlineUsers);
+	//   console.log(allUsers);
+	// for (var j = 0; j < onlineUsers.length; j++) {
+	//     if (allUsers.indexOf(onlineUsernames[j]) === -1){
+	//       offlineUsers.push(onlineUsernames[j]);
+	//     }
+	//   }
+	// console.log(offlineUsers)
 
 
 	$.ajax({
@@ -65,7 +67,7 @@ for (var i = 0; i < allUsers.length; i++) {
 		   console.log(data);
 	       		var resArr = data;
 	       		for (var r = 0; r < 1; r++) {
-       				offlineUsers.push(new User(resArr.display_name, resArr._id, resArr.game, resArr.url, resArr.logo));
+       				offlineUsers.push(new User(resArr.display_name, resArr._id, resArr.game, resArr.url, resArr.logo, resArr.delay));
        				html = '<li class="offline"><a href=' + resArr.url + ' target="_blank"><img class="logo" src=' + resArr.logo+ ">  " + resArr.display_name + '</a> offline</li>';
        				$(".users").append(html);
        				console.log(offlineUsers);	
